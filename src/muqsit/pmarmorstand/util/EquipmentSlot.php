7605c88393bc52e56e0f8cfff67a9189360c7897
NuhDev <92098247+NuhDevX@users.noreply.github.com>
2025-01-23 20:00:25 +0700

    Create EquipmentSlot.php

---
+++ src/muqsit/pmarmorstand/util/EquipmentSlot.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace muqsit\pmarmorstand\util;
+
+class EquipmentSlot{
+
+	public const MAINHAND = 0;
+	public const OFFHAND = 1;
+
+	public const HEAD = 0;
+	public const CHEST = 1;
+	public const LEGS = 2;
+	public const FEET = 3;
+}
