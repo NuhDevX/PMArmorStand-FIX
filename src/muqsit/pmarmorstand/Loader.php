ac656121210d3f87c69801feb96e9d70229f71cc
NuhDev <92098247+NuhDevX@users.noreply.github.com>
2025-01-24 08:04:43 +0700

    Update Loader.php

--- src/muqsit/pmarmorstand/Loader.php
+++ src/muqsit/pmarmorstand/Loader.php
@@ -9,13 +9,10 @@ use muqsit\pmarmorstand\entity\ArmorStandEntity;
 use muqsit\pmarmorstand\event\PlayerChangeArmorStandPoseEvent;
 use muqsit\pmarmorstand\pose\ArmorStandPoseRegistry;
 use muqsit\pmarmorstand\vanilla\ExtraVanillaData;
-use muqsit\simplepackethandler\SimplePacketHandler;
 use pocketmine\entity\EntityDataHelper;
 use pocketmine\entity\EntityFactory;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\network\mcpe\NetworkSession;
-use pocketmine\network\mcpe\protocol\InventoryTransactionPacket;
-use pocketmine\network\mcpe\protocol\types\inventory\UseItemOnEntityTransactionData;
 use pocketmine\plugin\PluginBase;
 use pocketmine\world\World;
 
@@ -34,40 +31,7 @@ final class Loader extends PluginBase{
 	}
 
 	protected function onEnable() : void{
-		SimplePacketHandler::createMonitor($this)->monitorIncoming(function(InventoryTransactionPacket $packet, NetworkSession $session) : void{
-			$player = $session->getPlayer();
-			if($player === null){
-				return;
-			}
-
-			$trData = $packet->trData;
-			if(!($trData instanceof UseItemOnEntityTransactionData) || $trData->getActionType() !== UseItemOnEntityTransactionData::ACTION_INTERACT){
-				return;
-			}
-
-			$world = $player->getWorld();
-			$entity = $world->getEntity($trData->getActorRuntimeId());
-			if(!($entity instanceof ArmorStandEntity)){
-				return;
-			}
-
-			$click_pos = $trData->getClickPosition();
-			if(!$player->canInteract($click_pos, 8) || !$entity->boundingBox->expandedCopy(0.25, 0.25, 0.25)->isVectorInside($click_pos)){
-				return;
-			}
-
-			if($player->isSneaking()){
-				$old_pose = $entity->getPose();
-				$new_pose = ArmorStandPoseRegistry::instance()->next($old_pose);
-				$ev = new PlayerChangeArmorStandPoseEvent($entity, $old_pose, $new_pose, $player);
-				$ev->call();
-				if(!$ev->isCancelled()){
-					$entity->setPose($ev->getNewPose());
-				}
-			}else{
-				$this->behaviour_registry->get($player->getInventory()->getItemInHand())->handleEquipment($player, $entity, $click_pos);
-			}
-		});
+		$this->getLogger("ambatukammm ah\nAmbatukammm");
 	}
 
 	public function getBehaviourRegistry() : ArmorStandBehaviourRegistry{
